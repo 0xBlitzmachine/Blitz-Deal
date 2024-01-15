04ed5c50cd874bea54ce65d1307b4108ada2937b
Blitzmachine <blitzm4ch1n3@gmail.com>
2024-01-15 20:46:15 +0100

    Initial commit - Xcode project.

---
+++ Blitz-Deal/Blitz-Deal/Blitz_DealApp.swift
@@ -0,0 +1,17 @@
+//
+//  Blitz_DealApp.swift
+//  Blitz-Deal
+//
+//  Created by Viktor Schäfer on 15.01.24.
+//
+
+import SwiftUI
+
+@main
+struct Blitz_DealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Blitz-Deal/Blitz-Deal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Blitz-Deal
+//
+//  Created by Viktor Schäfer on 15.01.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
